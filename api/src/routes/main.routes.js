fd6bbad60b66a624182481a477b526dc5f9fdc1f
Jorge Alves <alvesdjorge@gmail.com>
2021-07-21 00:34:16 -0300

    logica checkout

--- api/src/routes/main.routes.js
+++ api/src/routes/main.routes.js
@@ -7,34 +7,35 @@ const Product = require('../models/product');
 const createSplitTransaction = require('../services/pagarme').createSplitTransaction;
 
 router.get('/petshops', async (req, res) => {
-    try {
-        const petshops = await Petshop.find();
-        res.json({ error: false, petshops });
-    } catch (err) {
-        res.json({ error: true, message: err.message });
-    }
+  try {
+    const petshops = await Petshop.find();
+    res.json({ error: false, petshops });
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
 });
 
 router.get('/petshop/:id', async (req, res) => {
-    try {
-        const petshop = await Petshop.findById(req.params.id);
-        let products = await Product.find({
-            petshop_id: petshop._id
-        });
+  try {
+    const petshop = await Petshop.findById(req.params.id);
+    let products = await Product.find({
+        petshop_id: petshop._id
+    }).populate('petshop_id', 'recipient_id');
 
-        res.json({ error: false, petshop: { ...petshop._doc, products } });
-    } catch (err) {
-        res.json({ error: true, message: err.message });
-    }
+    res.json({ error: false, petshop: { ...petshop._doc, products } });
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
 });
 
 router.post('/porchase', async (req, res) => {
-    try {
-        const transaction = await createSplitTransaction(req.body);
-        res.json(transaction);
-    } catch (err) {
-        res.json({ error: true, message: err.message });
+  try {
+    const transaction = await createSplitTransaction(req.body);
+    res.json(transaction);
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
 }
-});
+);
 
 module.exports = router;
